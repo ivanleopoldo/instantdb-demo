7bf6038839bb37c4b255c3d5167fd7cd38d5dd50
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-04-15 00:39:27 +0800

    feat: adds not found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,5 +1,19 @@
-import { View } from 'react-native';
+import { Stack, router } from 'expo-router';
+import { Button, SafeAreaView, Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
-  return <View />;
+  return (
+    <SafeAreaView className="min-h-screen items-center justify-center">
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <View className="items-center">
+        <Text className="text-5xl font-bold">404</Text>
+        <Text className="font-light">PAGE NOT FOUND</Text>
+        <Button title="Go back Home" onPress={() => router.back()} />
+      </View>
+    </SafeAreaView>
+  );
 }
