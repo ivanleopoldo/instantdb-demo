baa580303f45bc57d36e514c1bf970c13ee37abf
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-04-15 01:36:15 +0800

    feat: adds textinputs in modal

--- app/create.tsx
+++ app/create.tsx
@@ -1,11 +1,21 @@
 import { Stack, router } from 'expo-router';
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import { Pressable } from 'react-native-gesture-handler';
 
 export default function CreateNote() {
-  const handleSave = () => {};
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+
+  const handleSave = () => {
+    alert(`Title: ${title}, Description: ${description}`);
+    if (router.canDismiss()) {
+      router.dismiss();
+    }
+  };
 
   return (
-    <View>
+    <View className="gap-6 p-4">
       <Stack.Screen
         options={{
           headerTitle: 'Create Note',
@@ -13,7 +23,31 @@ export default function CreateNote() {
           headerRight: () => <Button title="Save" onPress={handleSave} />,
         }}
       />
-      <Text>CreateNote</Text>
+      <View className="gap-2">
+        <View className="gap-2">
+          <Text className="text-sm font-light text-neutral-600">TITLE</Text>
+          <TextInput
+            placeholder="Title"
+            value={title}
+            onChangeText={setTitle}
+            className="rounded-lg bg-neutral-200 px-4 py-2"
+          />
+        </View>
+        <View className="gap-2">
+          <Text className="text-sm font-light text-neutral-600">DESCRIPTION</Text>
+          <TextInput
+            placeholder="Description"
+            value={description}
+            onChangeText={setDescription}
+            className="rounded-lg bg-neutral-200 px-4 py-2"
+          />
+        </View>
+      </View>
+      <Pressable onPress={handleSave}>
+        <View className="items-center rounded-lg bg-green-600 px-4 py-2">
+          <Text className="text-white">Submit</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
